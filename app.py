f8255d0e06aa8486ca624b2adbbc56ca1cc77bff
Javier <javier.has.no.life@gmail.com>
2023-05-10 19:07:37 +0800

    progress on frontend and backend 🚀

--- app.py
+++ app.py
@@ -1,29 +1,44 @@
 import requests
 from flask import Flask, render_template
+import mcstatus
 
 app = Flask(__name__,template_folder='template')
 
 @app.route("/")
-def ping_map():
-    url = "https://map.diffusehyperion.me"
+def index():
+    map_url = "https://map.diffusehyperion.me"
+    server_url = "mc.diffusehyperion.me"
 
     try:
-        response = requests.get(url)
+        response = requests.get(map_url)
         response.raise_for_status()
 
-        status = "Online!"
-        status_class = "success"
+        map_status = "Online"
+        map_status_class = "success"
     except requests.exceptions.HTTPError as err:
         code = int(str(err)[:3])
+        map_status=""
 
-        status = ""
         if code == 502:
-            status = "Offline..."
+            map_status = "Offline"
         else:
-            status = "Catastrophically Offline!"
-        status_class = "error"
+            map_status = "Catastrophically Offline"
+        map_status_class = "error"
 
-    return render_template("index.html", status_class=status_class, status=status)
+    try:
+        server = mcstatus.MinecraftServer.lookup(server_url)
+        status = server.status()
+        server_status = "Online"
+        server_status_class = "success"
+    except:
+        server_status = "Offline"
+        server_status_class = "error"
+
+    return render_template("index.html", map_status_class=map_status_class, map_status=map_status, server_status_class=server_status_class, server_status=server_status)
+
+@app.route('/info.html')
+def info():
+    return render_template("info.html")
 
 if __name__ == "__main__":
     app.run()
