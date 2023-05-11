1de0d1b33e2b2ed471d56eeae420f3fd8a27ce00
DiffuseHyperion <95083897+DiffuseHyperion@users.noreply.github.com>
2023-05-11 14:31:49 +0000

    more progress on back/frontend :)

--- app.py
+++ app.py
@@ -19,22 +19,48 @@ def index():
         code = int(str(err)[:3])
         map_status = ""
 
+        # Main Server offline
         if code == 502:
             map_status = "Offline!"
         else:
-            map_status = "Catastrophically Offline!!"
+            map_status = "Catastrophically Offline!"
+        map_status_class = "error"
+    # nginx record disabled
+    except requests.exceptions.SSLError as err:
+        map_status = "Catastrophically Offline!"
         map_status_class = "error"
 
+    server_online = False
     try:
         server = JavaServer.lookup(server_url)
         status = server.ping()
-        server_status = "Offline!"
-        server_status_class = "error"
-    except:
+
         server_status = "Online!"
         server_status_class = "success"
+        server_online = True
+    except:
+        server_status = "Offline!"
+        server_status_class = "error"
+        server_online = False
+    
+    if server_online:
+        server = JavaServer.lookup(server_url)
+        server_player_list = []
+        try:
+            server_player_list = server.query().players.names
+        except OSError:
+            pass
 
-    return render_template("index.html", map_status_class=map_status_class, map_status=map_status, server_status_class=server_status_class, server_status=server_status)
+        server_player_string = ""
+        if len(server_player_list) == 0:
+            server_player_string = "Nobody"
+        elif len(server_player_list) == 1:
+            server_player_string = server_player_list[0]
+        server_ping = server.ping()
+
+        return render_template("index.html", map_status_class=map_status_class, map_status=map_status, server_status_class=server_status_class, server_status=server_status, server_player_list=server_player_string, server_ping=server_ping)
+    else:
+        return render_template("index.html", map_status_class=map_status_class, map_status=map_status, server_status_class=server_status_class, server_status=server_status)
 
 
 @app.route('/info.html')
