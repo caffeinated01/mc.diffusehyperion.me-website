785313cb4816c5c8b98f605b35a9170869002ed8
DiffuseHyperion <95083897+DiffuseHyperion@users.noreply.github.com>
2023-05-11 10:45:18 +0000

    some frontend changes (sorry javier :))

--- app.py
+++ app.py
@@ -13,25 +13,25 @@ def index():
     try:
         response = requests.get(map_url)
         response.raise_for_status()
-        map_status = "Online"
+        map_status = "Online!"
         map_status_class = "success"
     except requests.exceptions.HTTPError as err:
         code = int(str(err)[:3])
         map_status = ""
 
         if code == 502:
-            map_status = "Offline"
+            map_status = "Offline!"
         else:
-            map_status = "Catastrophically Offline"
+            map_status = "Catastrophically Offline!!"
         map_status_class = "error"
 
     try:
         server = JavaServer.lookup(server_url)
         status = server.ping()
-        server_status = "Offline"
+        server_status = "Offline!"
         server_status_class = "error"
     except:
-        server_status = "Online"
+        server_status = "Online!"
         server_status_class = "success"
 
     return render_template("index.html", map_status_class=map_status_class, map_status=map_status, server_status_class=server_status_class, server_status=server_status)
