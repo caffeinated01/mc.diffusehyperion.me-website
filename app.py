b2d8bd40aa86f6abd9c4d7c516596ab206c20087
Javier <javier.has.no.life@gmail.com>
2023-05-11 17:04:45 +0800

    took me 30 mins to fix one line :)

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import requests
 from flask import Flask, render_template
-import mcstatus
+from mcstatus import JavaServer
 
-app = Flask(__name__,template_folder='template')
+app = Flask(__name__)
+
 
 @app.route("/")
 def index():
@@ -12,12 +13,11 @@ def index():
     try:
         response = requests.get(map_url)
         response.raise_for_status()
-
         map_status = "Online"
         map_status_class = "success"
     except requests.exceptions.HTTPError as err:
         code = int(str(err)[:3])
-        map_status=""
+        map_status = ""
 
         if code == 502:
             map_status = "Offline"
@@ -26,19 +26,21 @@ def index():
         map_status_class = "error"
 
     try:
-        server = mcstatus.MinecraftServer.lookup(server_url)
-        status = server.status()
-        server_status = "Online"
-        server_status_class = "success"
-    except:
+        server = JavaServer.lookup(server_url)
+        status = server.ping()
         server_status = "Offline"
         server_status_class = "error"
+    except:
+        server_status = "Online"
+        server_status_class = "success"
 
     return render_template("index.html", map_status_class=map_status_class, map_status=map_status, server_status_class=server_status_class, server_status=server_status)
+
 
 @app.route('/info.html')
 def info():
     return render_template("info.html")
+
 
 if __name__ == "__main__":
     app.run()
